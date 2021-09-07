0f2dfad058c55c39fe0e5be721db6d9a64aeab6e
miategeris <miategeris@gmail.com>
2021-09-07 09:54:23 -0400

    Finished shapes and star

---
+++ star.py
@@ -0,0 +1,19 @@
+import turtle
+
+
+turtle.penup()
+turtle.right(60)
+turtle.forward(300)
+turtle.pendown()
+turtle.setheading(0)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+
+turtle.exitonclick()
