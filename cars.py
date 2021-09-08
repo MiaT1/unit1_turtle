3cb55c4745e1877964b320099ad10ba9d667f7b7
miategeris <miategeris@gmail.com>
2021-09-08 11:03:52 -0400

    9/8 shapes

---
+++ cars.py
@@ -0,0 +1,16 @@
+import turtle
+
+# number of rotations
+for x in range(5):
+    for x in range(4):
+        turtle.forward(100)
+        turtle.left(90)
+    turtle.left(20)
+
+
+
+
+
+
+
+turtle.exitonclick()
---
+++ shape2.py
@@ -0,0 +1,9 @@
+import turtle
+
+for x in range(6):
+    for x in range(6):
+        turtle.forward(100)
+        turtle.left(60)
+    turtle.left(60)
+
+turtle.exitonclick()
