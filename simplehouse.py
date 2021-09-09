0ec12f791a227abe3723ac70c2535da51e4761e9
miategeris <miategeris@gmail.com>
2021-09-09 11:23:00 -0400

    completed simple house

---
+++ simplehouse.py
@@ -0,0 +1,30 @@
+import turtle
+
+
+for x in range (4):
+    turtle.forward(100)
+    turtle.right(90)
+
+for x in range (1):
+    turtle.left(60)
+    turtle.forward(100)
+    turtle.right(120)
+    turtle.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.exitonclick()
