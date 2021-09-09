2566b8e0b87d9254db6dbe2b84771acf08f1b594
miategeris <miategeris@gmail.com>
2021-09-09 11:39:42 -0400

    house is now colored in

--- simplehouse.py
+++ simplehouse.py
@@ -1,15 +1,31 @@
 import turtle
 
+turtle.color("blue")
+turtle.begin_fill()
 
-for x in range (4):
+
+for x in range(4):
     turtle.forward(100)
     turtle.right(90)
 
-for x in range (1):
+turtle.end_fill()
+
+turtle.color("red")
+turtle.begin_fill()
+
+
+for x in range(1):
     turtle.left(60)
     turtle.forward(100)
     turtle.right(120)
     turtle.forward(100)
+
+
+turtle.end_fill()
+
+
+
+
 
 
 
